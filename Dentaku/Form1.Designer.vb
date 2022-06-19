cf89c8deb07af4d6b23a31d56a1b7fdf4ebc7a3d
Tatsuya HIRANO <elko7954@gmail.com>
2022-06-19 15:14:51 +0900

    フォーム作成

--- Dentaku/Form1.Designer.vb
+++ Dentaku/Form1.Designer.vb
@@ -22,10 +22,267 @@ Partial Class Form1
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.txtShowOperator = New System.Windows.Forms.RichTextBox()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnPeriod = New System.Windows.Forms.Button()
+        Me.btnTimes = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnDevide = New System.Windows.Forms.Button()
+        Me.btnPlus = New System.Windows.Forms.Button()
+        Me.btnMinus = New System.Windows.Forms.Button()
+        Me.btnEqual = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Enabled = False
+        Me.RichTextBox1.Font = New System.Drawing.Font("游ゴシック", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(25, 77)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(398, 91)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'txtShowOperator
+        '
+        Me.txtShowOperator.Enabled = False
+        Me.txtShowOperator.Font = New System.Drawing.Font("游ゴシック", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.txtShowOperator.Location = New System.Drawing.Point(25, 29)
+        Me.txtShowOperator.Name = "txtShowOperator"
+        Me.txtShowOperator.Size = New System.Drawing.Size(398, 42)
+        Me.txtShowOperator.TabIndex = 1
+        Me.txtShowOperator.Text = ""
+        '
+        'btn7
+        '
+        Me.btn7.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btn7.Location = New System.Drawing.Point(25, 199)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(70, 52)
+        Me.btn7.TabIndex = 8
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btn8.Location = New System.Drawing.Point(101, 199)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(70, 52)
+        Me.btn8.TabIndex = 9
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btn9.Location = New System.Drawing.Point(177, 199)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(70, 52)
+        Me.btn9.TabIndex = 10
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btn6.Location = New System.Drawing.Point(177, 257)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(70, 52)
+        Me.btn6.TabIndex = 7
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btn5.Location = New System.Drawing.Point(101, 257)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(70, 52)
+        Me.btn5.TabIndex = 6
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btn4.Location = New System.Drawing.Point(25, 257)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(70, 52)
+        Me.btn4.TabIndex = 5
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btn3.Location = New System.Drawing.Point(177, 315)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(70, 52)
+        Me.btn3.TabIndex = 4
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btn2.Location = New System.Drawing.Point(101, 315)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(70, 52)
+        Me.btn2.TabIndex = 3
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btn1.Location = New System.Drawing.Point(25, 315)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(70, 52)
+        Me.btn1.TabIndex = 2
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btn0.Location = New System.Drawing.Point(25, 373)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(146, 52)
+        Me.btn0.TabIndex = 1
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'btnPeriod
+        '
+        Me.btnPeriod.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnPeriod.Location = New System.Drawing.Point(177, 373)
+        Me.btnPeriod.Name = "btnPeriod"
+        Me.btnPeriod.Size = New System.Drawing.Size(70, 52)
+        Me.btnPeriod.TabIndex = 11
+        Me.btnPeriod.Text = "."
+        Me.btnPeriod.UseVisualStyleBackColor = True
+        '
+        'btnTimes
+        '
+        Me.btnTimes.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnTimes.Location = New System.Drawing.Point(277, 257)
+        Me.btnTimes.Name = "btnTimes"
+        Me.btnTimes.Size = New System.Drawing.Size(70, 52)
+        Me.btnTimes.TabIndex = 12
+        Me.btnTimes.Text = "×"
+        Me.btnTimes.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(277, 199)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(146, 52)
+        Me.btnClear.TabIndex = 16
+        Me.btnClear.Text = "CA"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnDevide
+        '
+        Me.btnDevide.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnDevide.Location = New System.Drawing.Point(353, 257)
+        Me.btnDevide.Name = "btnDevide"
+        Me.btnDevide.Size = New System.Drawing.Size(70, 52)
+        Me.btnDevide.TabIndex = 13
+        Me.btnDevide.Text = "÷"
+        Me.btnDevide.UseVisualStyleBackColor = True
+        '
+        'btnPlus
+        '
+        Me.btnPlus.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnPlus.Location = New System.Drawing.Point(277, 315)
+        Me.btnPlus.Name = "btnPlus"
+        Me.btnPlus.Size = New System.Drawing.Size(70, 52)
+        Me.btnPlus.TabIndex = 14
+        Me.btnPlus.Text = "+"
+        Me.btnPlus.UseVisualStyleBackColor = True
+        '
+        'btnMinus
+        '
+        Me.btnMinus.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnMinus.Location = New System.Drawing.Point(353, 315)
+        Me.btnMinus.Name = "btnMinus"
+        Me.btnMinus.Size = New System.Drawing.Size(70, 52)
+        Me.btnMinus.TabIndex = 15
+        Me.btnMinus.Text = "ー"
+        Me.btnMinus.UseVisualStyleBackColor = True
+        '
+        'btnEqual
+        '
+        Me.btnEqual.Font = New System.Drawing.Font("游ゴシック", 19.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.btnEqual.Location = New System.Drawing.Point(277, 373)
+        Me.btnEqual.Name = "btnEqual"
+        Me.btnEqual.Size = New System.Drawing.Size(146, 52)
+        Me.btnEqual.TabIndex = 0
+        Me.btnEqual.Text = "＝"
+        Me.btnEqual.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(448, 449)
+        Me.Controls.Add(Me.btnEqual)
+        Me.Controls.Add(Me.btnMinus)
+        Me.Controls.Add(Me.btnPlus)
+        Me.Controls.Add(Me.btnDevide)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnTimes)
+        Me.Controls.Add(Me.btnPeriod)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.txtShowOperator)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents txtShowOperator As RichTextBox
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btnPeriod As Button
+    Friend WithEvents btnTimes As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnDevide As Button
+    Friend WithEvents btnPlus As Button
+    Friend WithEvents btnMinus As Button
+    Friend WithEvents btnEqual As Button
 End Class
