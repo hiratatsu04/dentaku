8276ec8963761da8da0a953bb4c89ec4a4d85e23
Tatsuya HIRANO <elko7954@gmail.com>
2022-06-29 20:45:43 +0900

    btn0をbtnxへ変更

--- Dentaku/Form1.vb
+++ Dentaku/Form1.vb
@@ -25,7 +25,7 @@
     Dim PreBtn As Integer = BtnType.ClearBtn    '一つ前に押されたボタンを格納する。上記の列挙型演算子を代入する
 
     '数ボタンが押された時の動作
-    Private Sub NumButtonClick(sender As Object, e As EventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click
+    Private Sub NumButtonClick(sender As Object, e As EventArgs) Handles btnx.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click
 
         Dim btnNumber = CType(sender, Button)   'senderをボタン型に変更
         Dim btnText As String = ""      'ボタンの[NAME]を格納する変数
